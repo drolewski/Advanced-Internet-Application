0828558821d1c44981926f65d4eec9fa6900690d
rolex <drolewski@gmail.com>
2020-05-14 02:06:06 +0200

    Add ASPMVC Tutorial

---
+++ ASPMVC/ASPMVC/Controllers/GenresController.cs
@@ -0,0 +1,101 @@
+﻿using ASPMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVC.Controllers
+{
+    public class GenresController : Controller
+    {
+
+        private MusicDbContext db = new MusicDbContext();
+
+        // GET: Genres
+        public ActionResult Index()
+        {
+            if (Request.IsAjaxRequest())
+                return PartialView("_SongsList", db.Genres.ToList());
+            else
+                return View(db.Genres.ToList());
+        }
+
+        // GET: Genres/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Genres/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Songs")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Genres.Add(genre);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(genre);
+        }
+
+
+        // GET: Genres/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Genre genre = db.Genres.Find(id);
+            if (genre == null)
+            {
+                return HttpNotFound();
+            }
+            return View(genre);
+        }
+
+
+        // POST: Genres/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Songs")] Genre genre)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(genre).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(genre);
+        }
+
+        // GET: Genres/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            var genre = await db.Genres
+                .FirstOrDefaultAsync(m => m.Id == id);
+            return View(genre);
+        }
+
+        // POST: Genres/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            var genre = await db.Genres.FindAsync(id);
+            db.Genres.Remove(genre);
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+
+
+}
---
+++ ASPMVC/ASPMVC/Controllers/SongsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ASPMVC.Models;
+
+namespace ASPMVC.Controllers
+{
+    public class SongsController : Controller
+    {
+        private MusicDbContext db = new MusicDbContext();
+
+        // GET: Songs
+        public ActionResult Index()
+        {
+            if (Request.IsAjaxRequest())
+                return PartialView("_SongsList", db.Songs.ToList());
+            else
+                return View(db.Songs.ToList());
+        }
+
+        // GET: Songs/Create
+        public ActionResult Create()
+        {
+            ViewBag.Genres = db.Genres.ToList();
+            return View();
+        }
+
+        // POST: Songs/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Artist,GenreId")] Song song)
+        {
+            ViewBag.Genres = db.Genres.ToList();
+            if (ModelState.IsValid)
+            {
+                db.Songs.Add(song);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(song);
+        }
+
+        // GET: Songs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Song song = db.Songs.Find(id);
+            if (song == null)
+            {
+                return HttpNotFound();
+            }
+            return View(song);
+        }
+
+        // POST: Songs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Artist,GenreId")] Song song)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(song).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(song);
+        }
+
+        // GET: Songs/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            var song = await db.Songs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            return View(song);
+        }
+
+        // POST: Songs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            var song = await db.Songs.FindAsync(id);
+            db.Songs.Remove(song);
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
